721ad6edfb4bd2f8c6796eb6982dc5b76c4551e5
snam <nam.s@chocomart.kz>
2022-10-05 16:24:41 +0600

    Initial Commit

---
+++ TraficLight/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TraficLight
+//
+//  Created by Сергей Нам on 05.10.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
