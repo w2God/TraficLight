f7fc6ac57e6a84e7570b9b131c810be7f388354b
snam <nam.s@chocomart.kz>
2022-10-05 17:39:03 +0600

    add action button

--- TraficLight/ViewController.swift
+++ TraficLight/ViewController.swift
@@ -9,11 +9,39 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var redView: UIView!
+    @IBOutlet var yellowView: UIView!
+    @IBOutlet var greenView: UIView!
+    
+    @IBOutlet var toggleButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        redView.backgroundColor = .red.withAlphaComponent(0.3)
+        yellowView.backgroundColor = .yellow.withAlphaComponent(0.3)
+        greenView.backgroundColor = .green.withAlphaComponent(0.3)
+        
         // Do any additional setup after loading the view.
     }
-
-
+    
+    
+    @IBAction func actionButton() {
+        if redView.backgroundColor == .red.withAlphaComponent(0.3) && yellowView.backgroundColor == .yellow.withAlphaComponent(0.3) && greenView.backgroundColor == .green.withAlphaComponent(0.3) || redView.backgroundColor == .red.withAlphaComponent(0.3) && yellowView.backgroundColor == .yellow.withAlphaComponent(0.3) && greenView.backgroundColor == .green {
+            redView.backgroundColor = .red
+            yellowView.backgroundColor = .yellow.withAlphaComponent(0.3)
+            greenView.backgroundColor = .green.withAlphaComponent(0.3)
+        } else if yellowView.backgroundColor == .yellow.withAlphaComponent(0.3) && redView.backgroundColor == .red {
+            yellowView.backgroundColor = .yellow
+            redView.backgroundColor = .red.withAlphaComponent(0.3)
+            greenView.backgroundColor = .green.withAlphaComponent(0.3)
+            
+        } else if greenView.backgroundColor == .green.withAlphaComponent(0.3) && yellowView.backgroundColor == .yellow {
+            greenView.backgroundColor = .green
+            redView.backgroundColor = .red.withAlphaComponent(0.3)
+            yellowView.backgroundColor = .yellow.withAlphaComponent(0.3)
+        }
+    }
+    
 }
 
